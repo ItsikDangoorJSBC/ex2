cc416ea8e4f029965e11db23625bac2e2ba28bf3
ItsikDangoorJSBC <itsik.dangoor3@gmail.com>
2018-04-22 16:47:45 +0300

    ssss

--- func.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function doCalc(a, b) {
-    return a + b;
-}
-exports.doCalc = doCalc;
-//# sourceMappingURL=func.js.map
--- runner.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var theFunc = require("./func");
-var res = theFunc.doCalc(80, 8);
-console.log(res);
-//# sourceMappingURL=runner.js.map
